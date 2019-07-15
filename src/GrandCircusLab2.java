9b7ad8a58ec073a24fd18415a08c08969df07d72
Kris Cherven <krischerven@gmail.com>
2019-07-15 15:25:19 -0400

    Initial commit

---
+++ src/GrandCircusLab2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class GrandCircusLab2 {
+	public static void main(String[] args) {
+		var s = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter the length of the classroom.");
+			var length = s.nextDouble();
+			System.out.println("Enter the width of the classroom.");
+			var width = s.nextDouble();
+			System.out.println("Room area: " + area(length, width));
+			System.out.println("Room perimeter: " + perimeter(length, width));
+			System.out.println("Continue? (y/n)");
+			s.nextLine(); // clear the buffer
+			var _cont = s.nextLine();
+			if (!_cont.toLowerCase().equals("y")) {
+				s.close();
+				break;
+			}
+		}
+	}
+	
+	private static double area(double width, double height) {
+		return width*height;
+	}
+	
+	private static double perimeter(double width, double height) {
+		return (2*width)+(2*height);
+	}
+	
+	
+}
